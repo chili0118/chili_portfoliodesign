fa3f550b5547c7ed18ea7437176001e1447cd9a6
chili0118 <b10236181@gapps.uch.edu.tw>
2021-02-03 21:36:35 +0800

    RWD+分頁設定

--- main.js
+++ main.js
@@ -7,4 +7,21 @@ $("#banner").slick({
 
 $("#works").slick({
     slidesToShow: 3,
-})
+    slidesToScroll: 2,
+    responsive: [
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+    ]
+});
